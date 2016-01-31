f97918a54899c990fc6ed278f670df3c45ee36cb
technosoftautomation <smchowdhury123@gmail.com>
2016-01-30 20:23:54 -0500

    8:30 is added

--- GitTutorial/src/main/java/com/maven/package_1/GitTutorial/Class1.java
+++ GitTutorial/src/main/java/com/maven/package_1/GitTutorial/Class1.java
@@ -6,6 +6,7 @@ public class Class1 {
 		System.out.println("This is class1 method1");
 		System.out.println("This is Friday");
 		System.out.println("2.5 PM");
+		System.out.println("This is 8:30PM");
 	}
 	
 }
